797f99deb6fb9fbf8e4022827bb3bcbe2e94b77f
Audrey C <audrey.cornaire@gmail.com>
2020-11-24 11:30:00 +0100

    [ADD] Architecture générale du projet, notebook et data_loader

---
+++ modules/data_loader.py
@@ -0,0 +1,83 @@
+import os
+from os import path, listdir, getcwd, makedirs
+
+import csv
+import requests
+import sys
+# import zipfile
+
+# import pandas as pd
+
+DATA_PATH = 'data/'
+DATASET_PATH = DATA_PATH + 'temperatures.csv'
+CURATED_LOCAL_PATH = DATA_PATH + 'curated/'
+COUNTRY = 'country/'
+REGION = 'region/'
+
+
+
+class DataSpliter:
+
+    def split_data_country(self):
+        '''
+        Break raw data into many files
+        '''
+
+        with open(DATASET_PATH, encoding='utf-8') as file_stream:    
+            file_stream_reader = csv.DictReader(file_stream, delimiter=',')
+            
+            open_files_references = {}
+
+            for row in file_stream_reader:
+                country = row['Country']
+                # Open a new file and write the header
+                if path.exists(CURATED_LOCAL_PATH + COUNTRY) == False:
+                    try:
+                        os.makedirs(CURATED_LOCAL_PATH + COUNTRY)
+                    except OSError:
+                        print ("Creation of the directory %s failed" % path)
+                        exit(1)
+
+                if country not in open_files_references:
+                    output_file = open(CURATED_LOCAL_PATH + COUNTRY +'{}.csv'.format(country), 'w', encoding='utf-8', newline='')
+                    dictionary_writer = csv.DictWriter(output_file, fieldnames=file_stream_reader.fieldnames)
+                    dictionary_writer.writeheader()
+                    open_files_references[country] = output_file, dictionary_writer
+                # Always write the row
+                open_files_references[country][1].writerow(row)
+            # Close all the files
+            for output_file, _ in open_files_references.values():
+                output_file.close()
+
+
+    def split_data_region(self):
+        '''
+        Break raw data into many files
+        '''
+
+        with open(DATASET_PATH, encoding='utf-8') as file_stream:    
+            file_stream_reader = csv.DictReader(file_stream, delimiter=',')
+            
+            open_files_references = {}
+
+            for row in file_stream_reader:
+                region = row['Region']
+                # Open a new file and write the header
+                if path.exists(CURATED_LOCAL_PATH + REGION) == False:
+                    try:
+                        os.makedirs(CURATED_LOCAL_PATH + REGION)
+                    except OSError:
+                        print ("Creation of the directory %s failed" % path)
+                        exit(1)
+
+                if region not in open_files_references:
+                    output_file = open(CURATED_LOCAL_PATH + REGION +'{}.csv'.format(region), 'w', encoding='utf-8', newline='')
+                    dictionary_writer = csv.DictWriter(output_file, fieldnames=file_stream_reader.fieldnames)
+                    dictionary_writer.writeheader()
+                    open_files_references[region] = output_file, dictionary_writer
+                # Always write the row
+                open_files_references[region][1].writerow(row)
+            # Close all the files
+            for output_file, _ in open_files_references.values():
+                output_file.close()
+
